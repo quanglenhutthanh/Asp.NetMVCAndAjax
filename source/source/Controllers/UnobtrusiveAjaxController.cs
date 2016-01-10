8cdaab255fa140a156f88fc0cf2261e78fc829da
lenhutthanhquang@gmail.com <lenhutthanhquang@gmail.com>
2016-01-10 22:36:52 +0700

    Unobtrusive Ajax - Ajax Form

---
+++ source/source/Controllers/UnobtrusiveAjaxController.cs
@@ -0,0 +1,36 @@
+﻿using Core;
+using Core.Reposities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace source.Controllers
+{
+    public class UnobtrusiveAjaxController : Controller
+    {
+        private GadgetRepository gadgetRepo = new GadgetRepository(new DataContext());
+        private CategoryRepository categoryRepo = new CategoryRepository(new DataContext());
+        //
+        // GET: /UnobtrusiveAjax/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult GadgetByCategoryData(int id)
+        {
+            var gadgets = gadgetRepo.Get();
+            if (id != -1)
+            {
+                gadgets = gadgetRepo.GetByCategoryId(id);
+            }
+            return PartialView(gadgets);
+        }
+
+        public ActionResult GadgetByCategory(int id = -1)
+        {
+            return View((object)id);
+        }
+	}
+}
